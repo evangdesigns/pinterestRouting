35aae6b9204315b91d47f3b654e4fa3359975ada
Evan Grabenstein <evangdesigns@gmail.com>
2020-01-04 09:33:48 -0600

    completed setup

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+        <button className="btn btn-primary" >CLICK ME!!</button>
+    </div>
+  );
+}
+
+export default App;

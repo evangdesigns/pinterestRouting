230fa76661d904912b68aad0f036f4c125dffe5c
Evan Grabenstein <evangdesigns@gmail.com>
2020-01-07 19:53:17 -0600

    added board component to home

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Board from '../../shared/Board/Board';
+
+import boardData from '../../../helpers/data/boardData';
+import authData from '../../../helpers/data/authData';
 
 import './Home.scss';
 
 class Home extends React.Component {
+  state = {
+    boards: [],
+  }
+
+  componentDidMount() {
+    boardData.getBoardsByUid(authData.getUid())
+      .then((boards) => this.setState({ boards }))
+      .catch((err) => console.error('errorfrom get boards', err));
+  }
+
   render() {
-    const boardId = '12345';
     return (
       <div className="Home">
         <h1>Home Page</h1>
-        <Link className="btn btn-secondary" to="/board/new">Create New Board</Link>
-        <Link className="btn btn-warning" to={`/board/${boardId}`}>Single Board Page</Link>
+        <div className="boards d-flex flex-wrap justify-content-center">
+          {this.state.boards.map((board) => <Board key={boardData.id} board={board} />)}
+        </div>
       </div>
     );
   }

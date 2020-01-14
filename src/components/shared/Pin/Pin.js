25937b495a9a364d05f57e236d1a6c15dea15bc6
Evan Grabenstein <evangdesigns@gmail.com>
2020-01-13 21:31:00 -0600

    editing pins

--- src/components/shared/Pin/Pin.js
+++ src/components/shared/Pin/Pin.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import PropTypes from 'prop-types';
 import pinShape from '../../../helpers/propz/pinShape';
@@ -28,7 +29,7 @@ class Pin extends React.Component {
             <img src={pin.imageUrl} alt={pin.name} />
           </div>
         <div className="card-footer">
-          <p><a href={pin.url}>VIEW PIN</a> | <button className="btn btn-link" onClick={this.deletePinEvent}>DELETE PIN</button></p>
+          <p><a href={pin.url}>VIEW PIN</a> | <button className="btn btn-link" onClick={this.deletePinEvent}>DELETE PIN</button> | <Link className="btn btn-link" to={`/board/${pin.boardId}/pin/${pin.id}/edit`}>EDIT PIN</Link></p>
         </div>
         </div>
       </div>
--- src/helpers/data/pinData.js
+++ src/helpers/data/pinData.js
@@ -28,9 +28,12 @@ const deletePin = (pinId) => axios.delete(`${baseUrl}/pins/${pinId}.json`);
 
 const savePin = (newPin) => axios.post(`${baseUrl}/pins.json`, newPin);
 
+const updatePin = (updatedPin, pinId) => axios.put(`${baseUrl}/pins/${pinId}.json`, updatedPin);
+
 export default {
   getPinsByBoardId,
   getSinglePin,
   deletePin,
   savePin,
+  updatePin,
 };
